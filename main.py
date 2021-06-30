2a13df91cf8791afd04284e62be92bde5cfdf34d
OCISLY-ds <83132050+OCISLY-ds@users.noreply.github.com>
2021-06-30 17:15:38 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+from luma.core.interface.serial import spi
+from luma.core.render import canvas
+from luma.lcd.device import st7735
+from PIL import Image, ImageDraw, ImageFont
+import time
+import pytz, random, os, json
+from datetime import datetime
+serial = spi(port=0, device=0, gpio_DC=23, gpio_RST=24)
+device = st7735(serial, rotate=2)
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 24)
+font1 = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 15)
+timezoneBerlin = pytz.timezone("Europe/Berlin")
+now = datetime.now(timezoneBerlin)
+current_time = now.strftime("%H\t%M\t%S")
+
+def Wassertemperatur():
+    # 1-wire Slave Datei lesen
+    file = open('/sys/bus/w1/devices/28-01193a114ec3/w1_slave')
+    filecontent = file.read()
+    file.close()
+
+    # Temperaturwerte auslesen und konvertieren
+    stringvalue = filecontent.split("\n")[1].split(" ")[9]
+    temperature = float(stringvalue[2:]) / 1000
+
+    # Temperatur ausgeben
+    rueckgabewert = '%6.2f' % temperature
+    return rueckgabewert
+
+def AuslassTemperatur():
+    # 1-wire Slave Datei lesen
+    file = open('/sys/bus/w1/devices/28-01193a1a932f/w1_slave')
+    filecontent = file.read()
+    file.close()
+
+    # Temperaturwerte auslesen und konvertieren
+    stringvalue = filecontent.split("\n")[1].split(" ")[9]
+    temperature = float(stringvalue[2:]) / 1000
+
+    # Temperatur ausgeben
+    rueckgabewert = '%6.2f' % temperature
+    return rueckgabewert
+
+# Box and text rendered in portrait mode
+with canvas(device) as draw:
+    draw.rectangle(device.bounding_box, outline="black", fill="black")
+    #draw.text((10, 10), "Uhrzeit:", current_time(), fill="white")
+    #while True:
+while 1:    
+    draw.text((10, 25), "Temperatur Auslass:", fill="red", font=font1)
+    draw.text((10, 45), (AuslassTemperatur()) + "°C", fill="red", font=font)
+    draw.text((10, 70), "Temperatur Wasser:", fill="blue", font=font1)
+    draw.text((10, 90), (Wassertemperatur()) + "°C", fill="blue", font=font)
+    device.clear()
+    time.sleep(10)
+ 
